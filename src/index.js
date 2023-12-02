a3eda4e10c8ee93a51d8c221b95825db728887a5
ManuelFBS <manuelf.borrego@gmail.com>
2023-12-02 00:26:31 -0400

    Actualización de index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello World !!!');
+});
+
 app.listen(7000);
 console.log('Server is running on port: ', 7000);
