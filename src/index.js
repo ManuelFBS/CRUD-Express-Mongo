9190781e5a81dba85e6c85836a70d764d47600cf
ManuelFBS <manuelf.borrego@gmail.com>
2023-12-02 01:24:38 -0400

    Actualización de package.json e index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
+import expresshbs from 'express-handlebars';
 import router from './routes/index.routes.js';
+import path from 'path';
 
 const app = express();
 
-app.set('port', process.env.PORT || 7000);
+// app.set('port', process.env.PORT || 7000);
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine(
+  '.hbs',
+  expresshbs({
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    defaultLayout: 'main',
+    extname: '.hbs'
+  })
+);
 
 // Routes -----------------------------------------------------------------------------------------------------
 app.use('/', router);
@@ -13,6 +25,9 @@ app.use('*', (req, res) => {
   });
 });
 
-app.listen(app.get('port'), () => {
-  console.log('Server is running on port: ', app.get('port'));
-});
+// app.listen(app.get('port'), () => {
+//   console.log('Server is running on port: ', app.get('port'));
+// });
+
+app.listen(7000);
+console.log('Server is running on port: ', 7000);
