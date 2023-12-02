81ecd10a463ba0cc53b206955e278ae0443c35df
ManuelFBS <manuelf.borrego@gmail.com>
2023-12-02 05:55:44 -0400

    Modificación dde index.routes.js

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,11 +3,11 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.send('<h1>Hello World !!!</h1>');
+  res.send('index');
 });
 
 router.get('/about', (req, res) => {
-  res.send('about...');
+  res.send('about');
 });
 
 export default router;
