5ce5d6ceb95e096b3d4290d2d75a1ec275859b82
ManuelFBS <manuelf.borrego@gmail.com>
2023-12-02 01:01:28 -0400

    Actualización de index.routes.js

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,7 +3,11 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello World !!!');
+  res.send('<h1>Hello World !!!</h1>');
+});
+
+router.get('/about', (req, res) => {
+  res.send('about...');
 });
 
 export default router;
