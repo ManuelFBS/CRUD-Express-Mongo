d4fbff43d3e0b865a8b44828e80b323075c96d11
ManuelFBS <manuelf.borrego@gmail.com>
2023-12-02 00:44:07 -0400

    Modificac. index.js y actual. index.routes.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import router from './routes/index.routes.js';
 
 const app = express();
 
-app.listen(7000);
-console.log('Server is running on port: ', 7000);
+app.set('port', process.env.PORT || 7000);
+
+// Routes -----------------------------------------------------------------------------------------------------
+app.use('/', router);
+app.use('*', (req, res) => {
+  res.status(404).json({
+    message: '404 - Not found...'
+  });
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Server is running on port: ', app.get('port'));
+});
