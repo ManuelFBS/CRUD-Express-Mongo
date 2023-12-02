0d21dad71e6a86af63d3eda4b172f664ae11f65f
ManuelFBS <manuelf.borrego@gmail.com>
2023-12-02 07:37:24 -0400

    Actuliz. .gitignore, packages(.json) y database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,11 @@
 import { connect } from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 (async () => {
   try {
-    const db = await connect('mongodb://localhost/crud-mongo');
+    const db = await connect(process.env.URL);
     console.log('DB connected to:', db.connection.name);
   } catch (error) {
     console.log(error);
