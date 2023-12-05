3f78d83441360a13357c7319d1f5b75ec279f538
ManuelFBS <manuelf.borrego@gmail.com>
2023-12-04 22:38:41 -0400

    Creación y actualización de task.controller.js

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,14 @@
+export const renderTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find().lean();
+    // Con esta función, se le agrega +1 al index...
+    const tasksWithIncrementedIndex = tasks.map((task, index) => {
+      return { ...task, displayIndex: index + 1 };
+    });
+
+    // res.render('index', { tasks: tasks });
+    res.render('index', { tasks: tasksWithIncrementedIndex });
+  } catch (error) {
+    console.log(error);
+  }
+};
