4011ebbfda335868c2876ba1e7c9f28541c842c8
ManuelFBS <manuelf.borrego@gmail.com>
2023-12-05 22:54:11 -0400

    Creación de app.js para redistribución de las funciones con respecto a index.js

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import './config/database.js';
+import exphbs from 'express-handlebars';
+import router from './routes/index.routes.js';
+import path from 'path';
+import morgan from 'morgan';
+
+export const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine(
+  '.hbs',
+  exphbs({
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    defaultLayout: 'main',
+    extname: '.hbs'
+  })
+);
+
+app.set('view engine', '.hbs');
+
+// Middlewares --------------------------------------------------------------------------------------------
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+
+// Routes -----------------------------------------------------------------------------------------------------
+app.use('/', router);
+app.use('*', (req, res) => {
+  res.status(404).json({
+    message: '404 - Not found...'
+  });
+});
+
+// app.listen(app.get('port'), () => {
+//   console.log('Server is running on port: ', app.get('port'));
+// });
--- src/index.js
+++ src/index.js
@@ -1,38 +1,6 @@
-import express from 'express';
-import './config/database.js';
-import exphbs from 'express-handlebars';
-import router from './routes/index.routes.js';
-import path from 'path';
-import morgan from 'morgan';
-
-const app = express();
+import { app } from './app.js';
 
 app.set('port', process.env.PORT || 7000);
-
-app.set('views', path.join(__dirname, 'views'));
-
-app.engine(
-  '.hbs',
-  exphbs({
-    layoutsDir: path.join(app.get('views'), 'layouts'),
-    defaultLayout: 'main',
-    extname: '.hbs'
-  })
-);
-
-app.set('view engine', '.hbs');
-
-// Middlewares --------------------------------------------------------------------------------------------
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: false }));
-
-// Routes -----------------------------------------------------------------------------------------------------
-app.use('/', router);
-app.use('*', (req, res) => {
-  res.status(404).json({
-    message: '404 - Not found...'
-  });
-});
 
 app.listen(app.get('port'), () => {
   console.log('Server is running on port: ', app.get('port'));
