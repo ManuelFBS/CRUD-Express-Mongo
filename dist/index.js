d17578ecc86d7d9de3e7bd6e18b7199c349262e1
ManuelFBS <manuelf.borrego@gmail.com>
2023-12-07 05:10:04 -0400

    Actualización de .gitignore

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _app = require("./app.js");
-_app.app.set('port', process.env.PORT || 7000);
-_app.app.listen(_app.app.get('port'), function () {
-  console.log('Server is running on port: ', _app.app.get('port'));
-});
